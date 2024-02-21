ecda62cab6b069646854234064e0664b25406733
Essa <essa700@hotmail.com>
2024-02-21 23:11:55 +0100

    name

--- app/src/main/java/se/home/finaappen/MainActivity.kt
+++ app/src/main/java/se/home/finaappen/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-            text = "jonas $name!",
+            text = "hej $name!",
             modifier = modifier
     )
 }
